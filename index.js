d520c2254ba4dd7fced4a0ba741a3087054478be
mbehboodian <29199390+xeptore@users.noreply.github.com>
2021-08-23 09:00:19 +0430

    feat: add github action launcher module

---
+++ index.js
@@ -0,0 +1,11 @@
+const core = require('@actions/core');
+const parse = require('./parse');
+
+try {
+  const version = core.getInput('version');
+  const { isPreRelease, tag } = parse(version);
+  core.setOutput("is-prerelease", JSON.stringify(isPreRelease));
+  core.setOutput("tag", tag);
+} catch (error) {
+  core.setFailed(error.message);
+}
